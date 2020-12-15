8f5df85ec19cf7c2d7534806ee397eb1e29e9d0a
molingyu <MolingyuYa@163.com>
2020-12-15 17:39:46 +0800

    首页,部分页面交互与展示

--- tb-x/src/views/Group/Group.vue
+++ tb-x/src/views/Group/Group.vue
@@ -1,44 +1,83 @@
+// 分类页面
 <template>
   <div class="group">
     <!-- 顶部返回及搜索定位 -->
     <van-sticky>
-      <van-nav-bar title="分类" left-text="返回" left-arrow>
+      <van-nav-bar
+        title="分类"
+        left-text="返回"
+        left-arrow
+        @click-left="onClickLeft"
+      >
         <template #right>
           <van-icon name="search" size="18" />
         </template>
       </van-nav-bar>
     </van-sticky>
-    <van-sidebar
-      v-model="activeKey"
-      style="width: 100px; font-size: 0.34rem; text-align: center"
-    >
-      <van-sidebar-item title="小米手机" />
-      <van-sidebar-item title="Redmi手机" />
-      <van-sidebar-item title="黑鲨手机" />
-      <van-sidebar-item title="电视" />
-      <van-sidebar-item title="大家电" />
-      <van-sidebar-item title="电脑办公" />
-      <van-sidebar-item title="小爱音箱" />
-      <van-sidebar-item title="路由器" />
-      <van-sidebar-item title="生活电器" />
-      <van-sidebar-item title="厨房电器" />
-      <van-sidebar-item title="智能穿戴" />
-      <van-sidebar-item title="智能家居" />
-      <van-sidebar-item title="车载出行" />
-      <van-sidebar-item title="个护健康" />
-    </van-sidebar>
+    <div class="group">
+      <ul class="wrap">
+        <li>小米手机</li>
+        <li>Redmi手机</li>
+        <li>黑鲨手机</li>
+        <li>电视</li>
+        <li>大家电</li>
+        <li>电脑办公</li>
+        <li>小爱音箱</li>
+        <li>路由器</li>
+        <li>小米手机</li>
+        <li>Redmi手机</li>
+        <li>黑鲨手机</li>
+        <li>电视</li>
+        <li>大家电</li>
+        <li>电脑办公</li>
+        <li>小爱音箱</li>
+        <li>路由器</li>
+        <li>路由器</li>
+        <li>小米手机</li>
+        <li>Redmi手机</li>
+        <li>黑鲨手机</li>
+        <li>电视</li>
+        <li>大家电</li>
+        <li>电脑办公</li>
+        <li>小爱音箱</li>
+        <li>路由器</li>
+      </ul>
+      <div class="groupdd"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Toast } from "vant";
 export default {
   data() {
     return {
       activeKey: 0,
     };
   },
+  methods: {
+    onClickLeft() {
+      this.$router.replace("/");
+    },
+    onClickRight() {
+      Toast("按钮");
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.wrap {
+  width: 6.25rem;
+  position: absolute;
+  left: 0;
+  top: 50px;
+  font-size: 1.1rem;
+  line-height: 2.7rem;
+  text-align: center;
+  flex: 1;
+  border-right: 0.0625rem solid #9999;
+}
+.wrap li {
+}
 </style>
